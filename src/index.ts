c69f3940966e8a808ed9229b64d36c88227cc4d3
Ricardo Fearing <ricardo@heysparkbox.com>
2022-02-18 08:59:02 -0500

    you know... webpack that joint

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,1 @@
-import express from 'express';
-
-const app = express();
-app.get('/', (req, res) => {
-  // Uncomment below to see error.
-  // let test = 'test';
-  res.send('Hello World!')
-})
-
-app.listen(4000, () => {
-  console.log(`server running on port 4000`);
-});
+console.log("Hello World!");
